c229536fcf6d3cc7f1b399686a9df18ee613dd1f
RSPatQ6IBI <exptoipiplusone@gmail.com>
2026-05-26 14:59:15 +0530

    Updated test_db_connection
    Updated the test file test_db_connection.py to ensure proper connection with the DB hosted at Clickhouse. The operation SELECT * from TABLE is performed to assert if all entries total to 10000.

--- tests/test_db_connection_.py
+++ tests/test_db_connection_.py
@@ -20,4 +20,18 @@ def test_db_connection_():
     the_clickhouse_client_ = get_client_()
     assert type(the_clickhouse_client_) == "<class 'clickhouse_connect.driver.httpclient.HttpClient'>"  
     
-    
+
+
+def test_db_communication_():
+    parent_dir = Path(__file__).resolve().parent.parent
+    config_path = str(parent_dir / "pyproject.toml").replace("\\", "/")
+    with open(config_path, "rb") as f:
+        data = tomllib.load(f)
+    tab_name_ = data["clickhouse-db"]["table_name_"]
+    from db_utils_.import_data_from_clickhouse_ import fetch_all_rows_ as fetch_rows_
+    from db_utils_ import db_commands_ as db_commands_
+    from db_utils_ .import_data_from_clickhouse_ import clickhouse_client_ as clickhouse_client_
+    a_client_ = clickhouse_client_()
+    fetch_data_query_ = db_commands_.fetch_all_entries_from_table(table_name_=tab_name_)
+    tab_result = a_client_.query_df(fetch_data_query_)
+    assert len(tab_result) == 10000
